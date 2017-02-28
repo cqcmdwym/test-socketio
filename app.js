864654e3611fbeb83b4b15f92d8ae997208b1756
chencoo <cookie.chen@autodesk.com>
2017-02-28 18:07:59 +0800

    chat room

--- app.js
+++ app.js
@@ -6,48 +6,25 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-var roomno=1;
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-
-io.on('connection',function(socket){
-	
-	
-	console.log('A user connected');
-	
-	//Send a message after a timeout of 4seconds
-	// setTimeout(function(){
-		// //socket.send('Sent a message 4 seconds after connection!');
-		// socket.emit('testerEvent',{
-			// description:'A custom event named testerEvent!'
-		// });
-	// }, 4000);
-	
-	
-	
-	//io.sockets.emit('broadcast',{description:clients+' clients connected!'});
-	
-	//socket.emit('newclientconnect',{ description: 'Hey, welcome!'});
-	
-	// socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
-	
-	// socket.on('clientEvent',function(data){
-		// console.log(data);
-	// });
-	
-	if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1){
-		roomno++;
-	}
-	socket.join('room-'+roomno);
-	
-	io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
-	
-	socket.on('disconnect',function(){
-		
-		console.log('A user disconnected');
-	});
+users = [];
+io.on('connection', function(socket){
+  console.log('A user connected');
+  socket.on('setUsername', function(data){
+    console.log(data);
+    if(users.indexOf(data) > -1){
+      socket.emit('userExists', data + ' username is taken! Try some other username.');
+    }
+    else{
+      users.push(data);
+      socket.emit('userSet', {username: data});
+    }
+  });
+  socket.on('msg', function(data){
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+  })
 });
-
